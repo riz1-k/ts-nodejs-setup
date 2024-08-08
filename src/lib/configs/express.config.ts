5113b7bfd66938499147b8611e2a31133688d52a
riz <rizwan.k.off@gmail.com>
2024-08-08 15:39:53 +0530

    updated intro page

--- src/lib/configs/express.config.ts
+++ src/lib/configs/express.config.ts
@@ -49,11 +49,10 @@ export function expressConfig(app: Application) {
           <meta charset="UTF-8">
           <meta http-equiv="X-UA-Compatible" content="IE=edge">
           <meta name="viewport" content="width=device-width, initial-scale=1.0">
-          <title>Habit Tracker</title>
+          <title>NodeJS API</title>
         </head>
         <body>
-          <h1>Habit Tracker</h1>
-          <p>Welcome to the Habit Tracker API</p>
+					<h1>Project is running successfully on port ${env.PORT}</h1>
         </body>
       </html>
     `
